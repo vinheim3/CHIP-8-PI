71b0c0912aa5553d2081e43e35b930726ab3cdfa
vinheim3 <dancjia@gmail.com>
2018-10-09 22:22:19 +0100

    Revert "remove use of quit and allowDraw, replace some exit's with return false, so we can check them and exit gracefully"
    This reverts commit 1ed117ef8ffc1f34c90b2f4a2544f3fb82cee3e6.

--- emulator.c
+++ emulator.c
@@ -18,6 +18,7 @@ int8_t      sound = -1;
 bool draw = false;
 bool key[16];
 bool paused = false;
+bool allowDraw;
 
 void initialize(void) {
     PC = 0x200;
@@ -45,14 +46,14 @@ void initialize(void) {
     memmove(memory, font, 80);
 }
 
-bool loadGame(const char *fileName) {
+void loadGame(const char *fileName) {
     FILE *file;
     uint16_t fileLen;
     
     file = fopen(fileName, "rb");
     if (!file) {
         printf("Unable to open binary file: %s", fileName);
-        return false;
+        exit(-1);
     }
     
     fseek(file, 0, SEEK_END);
@@ -64,7 +65,7 @@ bool loadGame(const char *fileName) {
     if (!buffer) {
         printf("Memory error!");
         fclose(file);
-        return false;
+        exit(-1);
     }
     
     fread(buffer, fileLen, 1, file);
@@ -72,27 +73,26 @@ bool loadGame(const char *fileName) {
     
     if (fileLen + PC > RAM) {
         printf("File too big. Size is: %d", fileLen + PC);
-        return false;
+        exit(-1);
     }
     
     for (int i = 0; i < fileLen; i++)
         memory[i + PC] = buffer[i];
     
     free(buffer);
-    return true;
 }
 
-bool emulatecycle(void) {
+void emulatecycle(void) {
     static uint8_t V[16], SP;
     static uint16_t opcode, I, stack[16];
     
-    static uint8_t x, y, kk;
+    static uint8_t x, y, kk, r;
     static uint16_t nnn;
     static int8_t keyPressed;
     
     if (PC >= RAM) {
         printf("End of RAM reached");
-        return false;
+        exit(-1);
     }
     
     opcode = memory[PC] << 8 | memory[PC+1];
@@ -104,116 +104,147 @@ bool emulatecycle(void) {
     switch (opcode & 0xF000) {
         case 0x0000:
             switch (kk) {
-                ///clear the display
-                case 0x00E0:
+                case 0x00E0: ///clear the display
+                    if (!allowDraw) break;
                     for (int i = 0; i < SCR_HEIGHT; i++)
                         for (int j = 0; j < SCR_WIDTH; j++)
                             screen[i][j] = false;
                     draw = true;
                     PC += 2;
                     break;
-                ///return from subroutine
-                case 0x00EE: PC = stack[--SP] + 2; break;
-                ///jump to machine code routine at nnn - old machines only
-                default: printf("Accessing old machine opcode.\n"); return false;
+                case 0x00EE: ///return from subroutine
+                    SP--;
+                    PC = stack[SP] + 2;
+                    break;
+                default: ///jump to machine code routine at nnn - old machines only
+                    printf("Accessing old machine opcode.\n");
+                    exit(-1);
             }
-
-        ///jump to location nnn
-        case 0x1000: PC = nnn; break;
-        ///call subroutine at nnn
-        case 0x2000: stack[SP++] = PC; PC = nnn; break;
-        ///skip next instruction if Vx is kk
-        case 0x3000:
-            if (V[x] == kk) PC += 4;
-            else            PC += 2;
             break;
-        ///skip next instruction if Vx is not kk
-        case 0x4000:
-            if (V[x] != kk) PC += 4;
-            else            PC += 2;
+        case 0x1000: ///jump to location nnn
+            PC = nnn;
             break;
-        case 0x5000:
-            ///skip next instruction if Vx is Vy
+        case 0x2000: ///call subroutine at nnn
+            stack[SP] = PC;
+            SP++;
+            PC = nnn;
+            break;
+        case 0x3000: ///skip next instruction if Vx is kk
+            if (V[x] == kk)
+                PC += 4;
+            else
+                PC += 2;
+            break;
+        case 0x4000: ///skip next instruction if Vx is not kk
+            if (V[x] != kk)
+                PC += 4;
+            else
+                PC += 2;
+            break;
+        case 0x5000: ///skip next instruction if Vx is Vy
             if ((opcode & 0x000F) == 0x0000) {
-                if (V[x] == V[y]) PC += 4;
-                else              PC += 2;
-                break;
+                if (V[x] == V[y])
+                    PC += 4;
+                else
+                    PC += 2;
             }
-
-            printf("Invalid opcode 0x5000.\n");
-            return false;
-
-        ///sets Vx to kk
-        case 0x6000: V[x] = kk; PC += 2; break;
-        ///add kk to Vx
-        case 0x7000: V[x] += kk; PC += 2; break;
+            else {
+                printf("Invalid opcode 0x5000.\n");
+                exit(-1);
+            }
+            break;
+        case 0x6000: ///sets Vx to kk
+            V[x] = kk;
+            PC += 2;
+            break;
+        case 0x7000: ///add kk to Vx
+            V[x] += kk;
+            PC += 2;
+            break;
         case 0x8000:
             switch (opcode & 0x000F) {
-                ///set Vx to Vy
-                case 0x0000: V[x] = V[y]; PC += 2; break;
-                ///set Vx to Vx | Vy
-                case 0x0001: V[x] |= V[y]; PC += 2; break;
-                ///set Vx to Vx & Vy
-                case 0x0002: V[x] &= V[y]; PC += 2; break;
-                ///set Vx to Vx ^ Vy
-                case 0x0003: V[x] ^= V[y]; PC += 2; break;
-                ///add Vy to Vx and set VF to 1 if there's a carry, 0 if not
-                case 0x0004:
-                    if (V[x] + V[y] > 0xFF) V[0xF] = 1;
-                    else                    V[0xF] = 0;
+                case 0x0000: ///set Vx to Vy
+                    V[x] = V[y];
+                    PC += 2;
+                    break;
+                case 0x0001: ///set Vx to Vx | Vy
+                    V[x] |= V[y];
+                    PC += 2;
+                    break;
+                case 0x0002: ///set Vx to Vx & Vy
+                    V[x] &= V[y];
+                    PC += 2;
+                    break;
+                case 0x0003: ///set Vx to Vx ^ Vy
+                    V[x] ^= V[y];
+                    PC += 2;
+                    break;
+                case 0x0004: ///add Vy to Vx and set VF to 1 if there's a carry, 0 if not
+                    if (V[x] + V[y] > 0xFF)
+                        V[0xF] = 1;
+                    else
+                        V[0xF] = 0;
                     V[x] = V[x] + V[y] - V[0xF]*0xFF;
                     PC += 2;
                     break;
-                ///subtract Vy from Vx and set VF to 1 if there's no borrow, 0 if there is
-                case 0x0005:
-                    if (V[y] > V[x]) V[0xF] = 0;
-                    else             V[0xF] = 1;
+                case 0x0005: ///subtract Vy from Vx and set VF to 1 if there's no borrow, 0 if there is
+                    if (V[y] > V[x])
+                        V[0xF] = 0;
+                    else
+                        V[0xF] = 1;
                     V[x] = V[x] - V[y] + (1-V[0xF])*0xFF;
                     PC += 2;
                     break;
-                ///Vx >> 1, VF is least significant bit (most-right) before
-                case 0x0006:
+                case 0x0006: ///Vx >> 1, VF is least significant bit (most-right) before
                     V[0xF] = (V[x] & 0x1);
                     V[x] >>= 1;
                     PC += 2;
                     break;
-                ///Vx is Vy-Vx and set VF to 1 if there's no borrow, 0 if there is
-                case 0x0007:
-                    if (V[x] > V[y]) V[0xF] = 0;
-                    else             V[0xF] = 1;
+                case 0x0007: ///Vx is Vy-Vx and set VF to 1 if there's no borrow, 0 if there is
+                    if (V[x] > V[y])
+                        V[0xF] = 0;
+                    else
+                        V[0xF] = 1;
                     V[x] = V[y] - V[x] + (1-V[0xF])*0xFF;
                     PC += 2;
                     break;
-                ///Vx << 1, VF is most significant bit (most-left)
-                case 0x000E:
+                case 0x000E: ///Vx << 1, VF is most significant bit (most-left)
                     V[0xF]  = (V[x] & 0x80) >> 7;
                     V[x] <<= 1;
-                    V[x] &= 0xFF;  // TODO: Do I need this if V[] is 8-bit?
+                    V[x] &= 0xFFFF;
                     PC += 2;
                     break;
                 default:
                     printf("Invalid opcode 0x8000.\n");
-                    return false;
+                    exit(-1);
             }
-        ///skip next instruction if Vx is not Vy
-        case 0x9000:
-            if (V[x] != V[y]) PC += 4;
-            else              PC += 2;
             break;
-        ///set I to address NNN
-        case 0xA000: I = nnn; PC += 2; break;
-        ///jump to address NNN+V0
-        case 0xB000: PC = nnn + V[0]; break;
-        ///Vx is kk & (random number)dest
-        case 0xC000: V[x] = kk & rand(); PC += 2; break;
-        ///draws sprites at I at Vx,Vy with N lines drawn
-        case 0xD000:
+        case 0x9000: ///skip next instruction if Vx is not Vy
+            if (V[x] != V[y])
+                PC += 4;
+            else
+                PC += 2;
+            break;
+        case 0xA000: ///set I to address NNN
+            I = nnn;
+            PC += 2;
+            break;
+        case 0xB000: ///jump to address NNN+V0
+            PC = nnn + V[0];
+            break;
+        case 0xC000: ///Vx is kk & (random number)dest
+            r = rand();
+            V[x] = kk & r;
+            PC += 2;
+            break;
+        case 0xD000: ///draws sprites at I at Vx,Vy with N lines drawn
+            if (!allowDraw) break;
             V[0xF] = 0;
             uint16_t pixel;
             for (int i = 0; i < (opcode & 0x000F); i++) {
                 pixel = memory[I + i];
                 for (int j = 0; j < 8; j++) {
-                    if ((pixel & (0x80 >> j)) != 0) {
+                    if ( (pixel & (0x80 >> j)) != 0) {
                         uint8_t screenYI = (V[y] + i) % SCR_HEIGHT;
                         uint8_t screenXI = (V[x] + j) % SCR_WIDTH;
                         if (screen[screenYI][screenXI] == 1)
@@ -227,8 +258,7 @@ bool emulatecycle(void) {
             break;
         case 0xE000:
             switch (kk) {
-                ///skip next instruction if key in Vx is pressed
-                case 0x009E:
+                case 0x009E: ///skip next instruction if key in Vx is pressed
                     if (key[V[x]]) {
                         PC += 4;
                         key[V[x]] = false;
@@ -236,8 +266,7 @@ bool emulatecycle(void) {
                     else
                         PC += 2;
                     break;
-                ///skip next instruction if key in Vx is not pressed
-                case 0x00A1:
+                case 0x00A1: ///skip next instruction if key in Vx is not pressed
                     if (!key[V[x]])
                         PC += 4;
                     else {
@@ -247,14 +276,16 @@ bool emulatecycle(void) {
                     break;
                 default:
                     printf("Invalid opcode 0xE000.\n");
-                    return false;
+                    exit(-1);
             }
+            break;
         case 0xF000:
             switch (kk) {
-                ///sets Vx to delay timer value
-                case 0x0007: V[x] = delay; PC += 2; break;
-                ///awaits a key press, then stores it in Vx
-                case 0x000A:
+                case 0x0007: ///sets Vx to delay timer value
+                    V[x] = delay;
+                    PC += 2;
+                    break;
+                case 0x000A: ///awaits a key press, then stores it in Vx
                     paused = true;
                     keyPressed = -1;
                     for (int i = 0; i < 16; i++) {
@@ -270,45 +301,44 @@ bool emulatecycle(void) {
                         PC += 2;
                     }
                     break;
-                ///sets delay timer to Vx
-                case 0x0015: delay = V[x]; PC += 2; break;
-                ///sets sound timer to Vx
-                case 0x0018: sound = V[x]; PC += 2; break;
+                case 0x0015: ///sets delay timer to Vx
+                    delay = V[x];
+                    PC += 2;
+                    break;
+                case 0x0018: ///sets sound timer to Vx
+                    sound = V[x];
+                    PC += 2;
+                    break;
                 case 0x001E: ///adds Vx to I
                     I += V[x];
                     if (I > 0xFFF)
-                        return false;
+                        exit(-1);
                     PC += 2;
                     break;
-                ///sets I to the location of font char in Vx
-                case 0x0029: I = V[x] * 5; PC += 2; break;
-                ///I, I+1, I+2 set to hundreds, tens and digits of decimal representation of Vx
-                case 0x0033:
+                case 0x0029: ///sets I to the location of font char in Vx
+                    I = V[x] * 5;
+                    PC += 2;
+                    break;
+                case 0x0033: ///I, I+1, I+2 set to hundreds, tens and digits of decimal representation of Vx
                     memory[I] = V[x] / 100;
                     memory[I + 1] = (V[x] / 10) % 10;
                     memory[I + 2] = V[x] % 10;
                     PC += 2;
                     break;
-                ///copies V0 to Vx in memory starting at I
-                case 0x0055:
+                case 0x0055: ///copies V0 to Vx in memory starting at I
                     for (int i = 0; i <= x; i++)
                         memory[I + i] = V[i];
                     PC += 2;
                     break;
-                ///fills V0 to Vx with values from memory starting from I
-                case 0x0065:
+                case 0x0065:///fills V0 to Vx with values from memory starting from I
                     for (int i = 0; i <= x; i++)
                         V[i] = memory[I + i];
                     PC += 2;
                     break;
                 default:
                     printf("Invalid opcode 0xF000.\n");
-                    return false;
+                    exit(-1);
             }
-        default:  // should not be reached
-            printf("Invalid opcode 0xF000.\n");
-            return false;
+            break;
     }
-
-    return true;
 }
